2b61908dc1f6ea00d4934d08e5b8c80cd958e48f
furlanvinicius <vinicius.furlan.espinha@gmail.com>
2016-08-24 14:25:40 -0300

    birl

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("foda-se marcos");
+            Console.ReadKey();
         }
     }
 }
